113f747bc4eb40f1333466cccf520c0350394bf4
Pavithr-Selvakumar <130045403+Pavithr-Selvakumar@users.noreply.github.com>
2023-04-07 19:58:25 +0530

    Update ALMRunBuilder.groovy

--- vars/ALMRunBuilder.groovy
+++ vars/ALMRunBuilder.groovy
@@ -11,10 +11,11 @@ def call (def testPathLocation)
                 echo 'There was some expected error during cleanup activity but it is handled.'
             }
             
+        echo {testPathLocation}
            catchError(buildResult:'UNSTABLE',catchInterruptions: true,message:'RSO Standard Catch Error', stageResult: 'UNSTABLE'){
                
                 runFromAlmBuilder almApiKey: '', almClientID: 'apikey-rpbedbkrhmrbnjsmkime', almCredentialsScope: 'SYSTEM', almDomain: 'JDE_TRAINING', almPassword: '', almProject: 'Kauri_Training', almRunHost: '', almRunMode: 'RUN_LOCAL', almServerName: 'JDEALM', 
-                  almTestSets: '''{testPathLocation}''', almUserName: '', areParametersEnabled: false, filterTestsModel: [blockedCheckbox: false, failedCheckbox: true, noRunCheckbox: false, notCompletedCheckbox: false, passedCheckbox: false, testName: ''], isSSOEnabled: true, specifyParametersModel: [parameterJson: '']
+                  almTestSets: '''Team_Alpha\\Digital Prospect Order\\A52. 1_DigitalWebShopOrder''', almUserName: '', areParametersEnabled: false, filterTestsModel: [blockedCheckbox: false, failedCheckbox: true, noRunCheckbox: false, notCompletedCheckbox: false, passedCheckbox: false, testName: ''], isSSOEnabled: true, specifyParametersModel: [parameterJson: '']
 				publishMicroFocusTestResults 'ALWAYS_ARCHIVE_TEST_REPORT'
                 echo currentBuild.result
 				
