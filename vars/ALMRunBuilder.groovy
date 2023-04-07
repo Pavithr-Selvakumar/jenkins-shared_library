51610a1ec14ac96def6e3fa417a3ab8b4bd3ccf6
Pavithr-Selvakumar <130045403+Pavithr-Selvakumar@users.noreply.github.com>
2023-04-07 18:41:36 +0530

    Update ALMRunBuilder.groovy

--- vars/ALMRunBuilder.groovy
+++ vars/ALMRunBuilder.groovy
@@ -14,7 +14,7 @@ def call (def testPathLocation)
            catchError(buildResult:'UNSTABLE',catchInterruptions: true,message:'RSO Standard Catch Error', stageResult: 'UNSTABLE'){
                
                 runFromAlmBuilder almApiKey: '', almClientID: 'apikey-rpbedbkrhmrbnjsmkime', almCredentialsScope: 'SYSTEM', almDomain: 'JDE_TRAINING', almPassword: '', almProject: 'Kauri_Training', almRunHost: '', almRunMode: 'RUN_LOCAL', almServerName: 'JDEALM', 
-                  almTestSets: '''{}''', almUserName: '', areParametersEnabled: false, filterTestsModel: [blockedCheckbox: false, failedCheckbox: true, noRunCheckbox: false, notCompletedCheckbox: false, passedCheckbox: false, testName: ''], isSSOEnabled: true, specifyParametersModel: [parameterJson: '']
+                  almTestSets: '''{testPathLocation}''', almUserName: '', areParametersEnabled: false, filterTestsModel: [blockedCheckbox: false, failedCheckbox: true, noRunCheckbox: false, notCompletedCheckbox: false, passedCheckbox: false, testName: ''], isSSOEnabled: true, specifyParametersModel: [parameterJson: '']
 				publishMicroFocusTestResults 'ALWAYS_ARCHIVE_TEST_REPORT'
                 echo currentBuild.result
 				
