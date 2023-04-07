86aa1c0e6a9ff6cdecef39dba3af7665085352d8
Pavithr-Selvakumar <130045403+Pavithr-Selvakumar@users.noreply.github.com>
2023-04-07 18:03:54 +0530

    Create ALMRunBuilder.groovy

---
+++ vars/ALMRunBuilder.groovy
@@ -0,0 +1,21 @@
+def ALMRunBuilder(testPathLocation){
+  script{    
+            try{
+                bat env.EndTask
+                bat env.TempClear
+                deleteDir()
+            }
+    
+            catch(err){
+                echo 'There was some expected error during cleanup activity but it is handled.'
+            }
+            
+           catchError(buildResult:'UNSTABLE',catchInterruptions: true,message:'RSO Standard Catch Error', stageResult: 'UNSTABLE'){
+               
+                runFromAlmBuilder almApiKey: '', almClientID: 'apikey-rpbedbkrhmrbnjsmkime', almCredentialsScope: 'SYSTEM', almDomain: 'JDE_TRAINING', almPassword: '', almProject: 'Kauri_Training', almRunHost: '', almRunMode: 'RUN_LOCAL', almServerName: 'JDEALM', 
+                  almTestSets: '''{testPathLocation}''', almUserName: '', areParametersEnabled: false, filterTestsModel: [blockedCheckbox: false, failedCheckbox: true, noRunCheckbox: false, notCompletedCheckbox: false, passedCheckbox: false, testName: ''], isSSOEnabled: true, specifyParametersModel: [parameterJson: '']
+                publishMicroFocusTestResults 'ALWAYS_ARCHIVE_TEST_REPORT'
+                echo currentBuild.result      
+            }
+        }
+}
