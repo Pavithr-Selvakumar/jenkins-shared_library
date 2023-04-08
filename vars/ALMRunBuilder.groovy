56792b77c4e766d3e860f55363798c142cbf48bc
Pavithr-Selvakumar <130045403+Pavithr-Selvakumar@users.noreply.github.com>
2023-04-08 20:37:02 +0530

    Update ALMRunBuilder.groovy

--- vars/ALMRunBuilder.groovy
+++ vars/ALMRunBuilder.groovy
@@ -15,11 +15,8 @@ def call (def testPathLocation)
 	 
          
                
-                runFromAlmBuilder almApiKey: '', almClientID: 'apikey-rpbedbkrhmrbnjsmkime', almCredentialsScope: 'SYSTEM', almDomain: 'JDE_TRAINING', almPassword: '', almProject: 'Kauri_Training', almRunHost: '', almRunMode: 'RUN_LOCAL', almServerName: 'JDEALM', 
-                  almTestSets: '''Team_Alpha\\Digital Prospect Order\\A52. 1_DigitalWebShopOrder''', almUserName: '', areParametersEnabled: false, filterTestsModel: [blockedCheckbox: false, failedCheckbox: true, noRunCheckbox: false, notCompletedCheckbox: false, passedCheckbox: false, testName: ''], isSSOEnabled: true, specifyParametersModel: [parameterJson: '']
-				publishMicroFocusTestResults 'ALWAYS_ARCHIVE_TEST_REPORT'
-                echo currentBuild.result
-				
+                runFromAlmBuilder almApiKey: '{AQAAABAAAAAgSreDziZexdVa5cVsr9NKyTLTpQEEiykPx57PMpBr/wcGAylXyZgAr3rC4zndURcY}', almClientID: ' apikey-rpbedbkrhmrbnjsmkime', almCredentialsScope: 'JOB', almDomain: 'JDE_TRAINING', almPassword: '{AQAAABAAAAAgadU6bQHk0s6997/ruOx2tFxaFd87MFJoFI2ZQh/Kbtb18xkoNqNUusbrKNpG3xlS}', almProject: 'Kauri_Training', almRunHost: '', almRunMode: 'RUN_LOCAL', almServerName: 'JDEALM', almTestSets: '''Team_Alpha\\\\Digital Prospect Order\\\\A52. 1_DigitalWebShopOrder
+''', almTimeout: '', almUserName: 'apikey-rpbedbkrhmrbnjsmkime', areParametersEnabled: false, filterTestsModel: [blockedCheckbox: false, failedCheckbox: false, noRunCheckbox: false, notCompletedCheckbox: false, passedCheckbox: false, testName: ''], isSSOEnabled: true, specifyParametersModel: [parameterJson: '']
               
               
               
