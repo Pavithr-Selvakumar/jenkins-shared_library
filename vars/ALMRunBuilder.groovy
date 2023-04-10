45b18d8b32dca5329e47f93101f793ad2533d9d8
Pavithr-Selvakumar <130045403+Pavithr-Selvakumar@users.noreply.github.com>
2023-04-10 16:17:03 +0530

    Update ALMRunBuilder.groovy

--- vars/ALMRunBuilder.groovy
+++ vars/ALMRunBuilder.groovy
@@ -11,6 +11,9 @@ def call (def testPathLocation)
             }
 		    
 		    echo testPathLocation
+		    
+		    runFromAlmBuilder almApiKey: '{AQAAABAAAAAggbQGiwiY1PPZpjHyCiPAuwEOyzLBNUB5lxBvbEqaML1FxjMlmVeuSnmBJ+zJJzCx}', almClientID: ' apikey-rpbedbkrhmrbnjsmkime', almCredentialsScope: 'JOB', almDomain: 'JDE_TRAINING', almPassword: '{AQAAABAAAAAQSr7Ql75DCLbDFcPYfYlp84TgGcrmQP/Kp9KN+ymvRrs=}', almProject: 'Kauri_Training', almRunHost: '', almRunMode: 'RUN_LOCAL', almServerName: 'JDEALM', almTestSets: '''Team_Alpha\\Digital Prospect Order\\A52. 1_DigitalWebShopOrder
+''', almTimeout: '', almUserName: 'apikey-rpbedbkrhmrbnjsmkime', areParametersEnabled: false, filterTestsModel: [blockedCheckbox: false, failedCheckbox: false, noRunCheckbox: false, notCompletedCheckbox: false, passedCheckbox: false, testName: ''], isSSOEnabled: true, specifyParametersModel: [parameterJson: '']
             
 	 
          
