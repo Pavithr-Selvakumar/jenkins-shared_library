d63ab0c3383e092cb9536908db463116b7dd82cb
Pavithr-Selvakumar <130045403+Pavithr-Selvakumar@users.noreply.github.com>
2023-04-12 16:54:10 +0530

    Update ALMRunBuilder.groovy

--- vars/ALMRunBuilder.groovy
+++ vars/ALMRunBuilder.groovy
@@ -10,9 +10,15 @@ def call (def testPathLocation)
                 echo 'There was some expected error during cleanup activity but it is handled.'
             }
 		    
-		    echo testPathLocation
+		    
 		    
 		    runFromAlmBuilder almApiKey: '{AQAAABAAAAAggbQGiwiY1PPZpjHyCiPAuwEOyzLBNUB5lxBvbEqaML1FxjMlmVeuSnmBJ+zJJzCx}', almClientID: 'apikey-rpbedbkrhmrbnjsmkime', almCredentialsScope: 'JOB', almDomain: 'JDE_TRAINING', almPassword: '{AQAAABAAAAAQSr7Ql75DCLbDFcPYfYlp84TgGcrmQP/Kp9KN+ymvRrs=}', almProject: 'Kauri_Training', almRunHost: '', almRunMode: 'RUN_LOCAL', almServerName: 'JDEALM', almTestSets: testPathLocation, almTimeout: '', almUserName: 'apikey-rpbedbkrhmrbnjsmkime', areParametersEnabled: false, filterTestsModel: [blockedCheckbox: false, failedCheckbox: false, noRunCheckbox: false, notCompletedCheckbox: false, passedCheckbox: false, testName: ''], isSSOEnabled: true, specifyParametersModel: [parameterJson: '']
+		    
+		   resultFile = findFiles excludes: '', glob: 'Results*.xml'
+                   def xml = readFile resultFile[0].name
+                   def xmlContents = new XmlParser().parseText(xml)
+                   status =((xmlContents.children())[0].children())[0].attributes().status
+		    return[teststatus:status]
             
 	 
          
