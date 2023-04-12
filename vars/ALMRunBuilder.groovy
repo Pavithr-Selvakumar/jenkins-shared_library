2707c2a143355b4898a74962c67e20309164b07e
Pavithr-Selvakumar <130045403+Pavithr-Selvakumar@users.noreply.github.com>
2023-04-12 17:30:57 +0530

    Update ALMRunBuilder.groovy

--- vars/ALMRunBuilder.groovy
+++ vars/ALMRunBuilder.groovy
@@ -19,6 +19,15 @@ def call (def testPathLocation)
                    def xmlContents = new XmlParser().parseText(xml)
                    status =((xmlContents.children())[0].children())[0].attributes().status
 		    if (status =="fail"){
+			    
+	try{
+                bat env.EndTask
+                bat env.TempClear
+                deleteDir()
+            }
+            catch(err){
+                echo 'There was some expected error during cleanup activity but it is handled.'
+            }
 		    runFromAlmBuilder almApiKey: '{AQAAABAAAAAggbQGiwiY1PPZpjHyCiPAuwEOyzLBNUB5lxBvbEqaML1FxjMlmVeuSnmBJ+zJJzCx}', almClientID: 'apikey-rpbedbkrhmrbnjsmkime', almCredentialsScope: 'JOB', almDomain: 'JDE_TRAINING', almPassword: '{AQAAABAAAAAQSr7Ql75DCLbDFcPYfYlp84TgGcrmQP/Kp9KN+ymvRrs=}', almProject: 'Kauri_Training', almRunHost: '', almRunMode: 'RUN_LOCAL', almServerName: 'JDEALM', almTestSets: testPathLocation, almTimeout: '', almUserName: 'apikey-rpbedbkrhmrbnjsmkime', areParametersEnabled: false, filterTestsModel: [blockedCheckbox: false, failedCheckbox: false, noRunCheckbox: false, notCompletedCheckbox: false, passedCheckbox: false, testName: ''], isSSOEnabled: true, specifyParametersModel: [parameterJson: '']
 			    
 		    }
