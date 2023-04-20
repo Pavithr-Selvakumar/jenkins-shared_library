7db3c10bc6191659cde44ac9ed6321323e70361e
Pavithr-Selvakumar <130045403+Pavithr-Selvakumar@users.noreply.github.com>
2023-04-20 15:39:34 +0530

    Update ALMRunBuilder.groovy

--- vars/ALMRunBuilder.groovy
+++ vars/ALMRunBuilder.groovy
@@ -1,26 +1,26 @@
 def call (def testPathLocation)
 { 
-            script{
-                try{
-                bat env.EndTask
-                bat env.TempClear
-                deleteDir()
-            }
-            catch(err){
-                echo 'There was some expected error during cleanup activity but it is handled.'
-            }
+    script{
+        try{
+            bat env.EndTask
+            bat env.TempClear
+            deleteDir()
+        }
+        catch(err){
+	    echo 'There was some expected error during cleanup activity but it is handled.'
+        }
 		    
 		    
+		runFromAlmBuilder almApiKey: '{AQAAABAAAAAggbQGiwiY1PPZpjHyCiPAuwEOyzLBNUB5lxBvbEqaML1FxjMlmVeuSnmBJ+zJJzCx}', almClientID: 'apikey-rpbedbkrhmrbnjsmkime', almCredentialsScope: 'JOB', almDomain: 'JDE_TRAINING', almPassword: '{AQAAABAAAAAQSr7Ql75DCLbDFcPYfYlp84TgGcrmQP/Kp9KN+ymvRrs=}', almProject: 'Kauri_Training', almRunHost: '', almRunMode: 'RUN_LOCAL', almServerName: 'JDEALM', almTestSets: testPathLocation, almTimeout: '', almUserName: 'apikey-rpbedbkrhmrbnjsmkime', areParametersEnabled: false, filterTestsModel: [blockedCheckbox: false, failedCheckbox: false, noRunCheckbox: false, notCompletedCheckbox: false, passedCheckbox: false, testName: ''], isSSOEnabled: true, specifyParametersModel: [parameterJson: '']    
 		    
-		    runFromAlmBuilder almApiKey: '{AQAAABAAAAAggbQGiwiY1PPZpjHyCiPAuwEOyzLBNUB5lxBvbEqaML1FxjMlmVeuSnmBJ+zJJzCx}', almClientID: 'apikey-rpbedbkrhmrbnjsmkime', almCredentialsScope: 'JOB', almDomain: 'JDE_TRAINING', almPassword: '{AQAAABAAAAAQSr7Ql75DCLbDFcPYfYlp84TgGcrmQP/Kp9KN+ymvRrs=}', almProject: 'Kauri_Training', almRunHost: '', almRunMode: 'RUN_LOCAL', almServerName: 'JDEALM', almTestSets: testPathLocation, almTimeout: '', almUserName: 'apikey-rpbedbkrhmrbnjsmkime', areParametersEnabled: false, filterTestsModel: [blockedCheckbox: false, failedCheckbox: false, noRunCheckbox: false, notCompletedCheckbox: false, passedCheckbox: false, testName: ''], isSSOEnabled: true, specifyParametersModel: [parameterJson: '']
-		    
-		   resultFile = findFiles excludes: '', glob: 'Results*.xml'
-                   def xml = readFile resultFile[0].name
-                   def xmlContents = new XmlParser().parseText(xml)
-                   status =((xmlContents.children())[0].children())[0].attributes().status
-		    if (status =="fail"){
+		resultFile = findFiles excludes: '', glob: 'Results*.xml'
+		def xml = readFile resultFile[0].name
+		def xmlContents = new XmlParser().parseText(xml)
+		status =((xmlContents.children())[0].children())[0].attributes().status    
+
+		if (status =="fail"){
 			    
-	try{
+			try{
                 bat env.EndTask
                 bat env.TempClear
                 deleteDir()
@@ -33,17 +33,7 @@ def call (def testPathLocation)
 		    }
 		   
 		    
-		    return [status]
-            
-	 
-         
-               
-             
-              
-              
-              
-              
-            
-            
-        }
+	    return [status]
+   
+    }
 }
