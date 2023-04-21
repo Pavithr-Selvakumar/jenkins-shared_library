496f4b99fd6cf733c6e0188bf23cb8d9abc5cf7b
Pavithr-Selvakumar <130045403+Pavithr-Selvakumar@users.noreply.github.com>
2023-04-21 18:06:51 +0530

    Update ALMRunBuilder.groovy

--- vars/ALMRunBuilder.groovy
+++ vars/ALMRunBuilder.groovy
@@ -20,7 +20,7 @@ def call (def testPathLocation)
 		def xml = readFile resultFile[0].name
 		def xmlContents = new XmlParser().parseText(xml)
 		status =((xmlContents.children())[0].children())[0].attributes().status    
-		
+	    echo 'first run ${status}'
 	    // rerun the same test if it fails 
 		if (status =="fail"){
 			    
@@ -33,13 +33,15 @@ def call (def testPathLocation)
                 echo 'There was some expected error during cleanup activity but it is handled.'
             }
 		 runFromAlmBuilder almApiKey: '{AQAAABAAAAAggbQGiwiY1PPZpjHyCiPAuwEOyzLBNUB5lxBvbEqaML1FxjMlmVeuSnmBJ+zJJzCx}', almClientID: 'apikey-rpbedbkrhmrbnjsmkime', almCredentialsScope: 'JOB', almDomain: 'JDE_TRAINING', almPassword: '{AQAAABAAAAAQSr7Ql75DCLbDFcPYfYlp84TgGcrmQP/Kp9KN+ymvRrs=}', almProject: 'Kauri_Training', almRunHost: '', almRunMode: 'RUN_LOCAL', almServerName: 'JDEALM', almTestSets: testPathLocation, almTimeout: '', almUserName: 'apikey-rpbedbkrhmrbnjsmkime', areParametersEnabled: false, filterTestsModel: [blockedCheckbox: false, failedCheckbox: false, noRunCheckbox: false, notCompletedCheckbox: false, passedCheckbox: false, testName: ''], isSSOEnabled: true, specifyParametersModel: [parameterJson: '']
-			    
-		    }
-		
-	        resultFile = findFiles excludes: '', glob: 'Results*.xml'
+			  resultFile = findFiles excludes: '', glob: 'Results*.xml'
 		 xml = readFile resultFile[0].name
 		 xmlContents = new XmlParser().parseText(xml)
-		status =((xmlContents.children())[0].children())[0].attributes().status   
+		status =((xmlContents.children())[0].children())[0].attributes().status 
+	     echo 'Second run ${status}' 
+			
+		    }
+		
+	      
 		    
 	    return [status]
    
